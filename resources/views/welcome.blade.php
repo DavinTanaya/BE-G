925a12c73c612633f81330f3acc4b516e564c6e6
DavinTanaya <davin.tanaya@gmail.com>
2024-11-21 19:01:43 +0700

    sesi1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>sesi 1</title>
+    <style>
+        h6 {
+            color: green;
+        }
+    </style>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <img src="https://th.bing.com/th/id/OIP.oclsmngXgMLCJBTkS_Z6MAHaJ2?w=201&h=268&c=7&r=0&o=5&dpr=2&pid=1.7" alt="Gambar Spongebob">
+    <h1>Judul 1</h1>
+    <h2>Judul 2</h2>
+    <h3>Judul 3</h3>
+    <h4>Judul 4</h4>
+    <h5>Judul 5</h5>
+    <h6>Judul 6</h6>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aperiam, dolore neque magni veritatis cupiditate, blanditiis accusamus soluta similique suscipit quod nihil eveniet labore, fugit aliquid sequi nostrum atque id.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit beatae cupiditate rerum itaque quis quas suscipit omnis debitis saepe voluptatum impedit, eum sint numquam neque unde? Ut ducimus accusamus hic.</p>
+    Teks <br>
+    <hr>
+    Teks
+    <a href="https://www.youtube.com">link ke youtube</a>
+
+    <form action="https://www.youtube.com">
+        <label for="name">Nama:</label>
+        <input type="text" id="name" name="name">
+        <button type="submit">Submit</button>
+    </form>
+
+    <table>
+        <tr>
+            <td>Nama</td>
+            <td>Nim</td>
+            <td>Kelas</td>
+        </tr>
+        <tr>
+            <td>Davin</td>
+            <td>123456789</td>
+            <td>LA05</td>
+        </tr>
+    </table>
+    <div style="color:blue;" class="footer">
+        <h6 class="judulfooter">Div 1</h6>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aperiam, dolore neque magni veritatis cupiditate, blanditiis accusamus soluta similique suscipit quod nihil eveniet labore, fugit aliquid sequi nostrum atque id.</p>
+    </div>
+    <div>
+        <h6 class="judulfooter">Div 2</h6>
+        <p id="pfooter">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aperiam, dolore neque magni veritatis cupiditate, blanditiis accusamus soluta similique suscipit quod nihil eveniet labore, fugit aliquid sequi nostrum atque id.</p>
+    </div>
+</body>
+</html>
